f1fa207ab1110373d2e604396eaf9e911537e867
zhiyongzaixian <13852292775@163.com>
2020-10-26 10:45:22 +0800

    '1024'

--- guiguyunyinyue_study/pages/personal/personal.js
+++ guiguyunyinyue_study/pages/personal/personal.js
@@ -1,6 +1,8 @@
 let startY = 0; // 手指起始坐标
 let moveY = 0; // 手指实时移动的坐标
 let moveDistance = 0; // 手指移动的距离
+
+import request from '../../utils/request'
 Page({
 
   /**
@@ -8,14 +10,39 @@ Page({
    */
   data: {
     coverTransform: 'translateY(0)',
-    coverTransition: ''
+    coverTransition: '',
+    userInfo: {}, // 用户信息
+    recentPlayList: [], // 用户最近播放记录
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // 获取用户信息
+    let userInfo = wx.getStorageSync('userInfo');
+    if(userInfo){
+      this.setData({
+        userInfo
+      })
+      this.getUserRecentPlay();
+    }
+   
+    
+    
+  },
+  
+  // 获取用户最近播放记录
+  async getUserRecentPlay(){
+    let recentPlayData = await request('/user/record', {uid: this.data.userInfo.userId,type: 0});
+    let id = 0;
+    let recentPlayList = recentPlayData.allData.slice(0, 10).map(item => {
+      item.id = id++;
+      return item;
+    });
+    this.setData({
+      recentPlayList
+    })
   },
   
   handleTouchStart(event){
@@ -46,6 +73,17 @@ Page({
     this.setData({
       coverTransform: `translateY(0rpx)`,
       coverTransition: 'transform 1s linear'
+    })
+  },
+  
+  // 跳转至login的回调
+  toLogin(){
+    // 判断用户是否登录
+    if(this.data.userInfo.nickname){
+      return;
+    }
+    wx.navigateTo({
+      url: '/pages/login/login'
     })
   },
 
