2f6bbe8634d5ae379f5ad4070922c02c7cd919cd
zhiyongzaixian <13852292775@163.com>
2020-10-30 17:00:57 +0800

    '1030'

--- guigushangcheng_study/guigushop/components/Recommend/Recommend.vue
+++ guigushangcheng_study/guigushop/components/Recommend/Recommend.vue
@@ -18,12 +18,39 @@
 				</view>
 			</swiper-item>
 		</swiper>
+	
+		<!-- 三个图标 policyDescList 区域  -->
+		<view class="policyDescList">
+			<view class="policyItem" v-for="item in indexData.policyDescList" :key='item.desc'>
+				<image :src="item.icon" mode=""></image>
+				<text>{{item.desc}}</text>
+			</view>
+		</view>
+		
+		<!-- 10个图标kingKongList 区域  -->
+		<view class="kingKongList" v-if="indexData.kingKongModule">
+			<view class="kingKongItem" v-for="item in indexData.kingKongModule.kingKongList" :key='item.L1Id'>
+				<image :src="item.picUrl" mode=""></image>
+				<text>{{item.text}}</text>
+			</view>
+		</view>
+		
+		<IndexCateGory></IndexCateGory>
 	</view>
 </template>
 
 <script>
+	import {mapState} from 'vuex'
+	import IndexCateGory from '../indexCateGory/indexCateGory.vue'
 	export default {
-		
+		components:{
+			IndexCateGory
+		},
+		computed: {
+			...mapState({
+				indexData: state => state.indexModule.indexData
+			})
+		}
 	}
 </script>
 
@@ -38,4 +65,34 @@
 				image
 					width 100%
 					height 100%
+					
+		.policyDescList
+			display flex
+			.policyItem
+				flex 1
+				text-align center
+				image
+					width 32rpx
+					height 32rpx
+					/* 设置纵向对其方式： 基于中间对齐 */
+					vertical-align middle
+					margin-right 20rpx
+				text
+					font-size 24rpx
+					vertical-align middle
+		.kingKongList
+			display flex
+			flex-wrap wrap
+			.kingKongItem
+				width 20%
+				display flex
+				flex-direction column
+				align-items center
+				image
+					width 110rpx
+					height 110rpx
+					margin 10rpx 0
+				text
+					font-size 26rpx
+			
 </style>
---
+++ guigushangcheng_study/guigushop/components/indexCateGory/indexCateGory.vue
@@ -0,0 +1,52 @@
+<template>
+	<view class="categoryList">
+		<view class="cateGoryItem" v-for="item in indexData.categoryModule" :key='item.titlePicUrl'>
+			<image class="titlePic" :src="item.titlePicUrl"></image>
+			<scroll-view scroll-x="true" class="cateScroll">
+				<view class="scrollItem" v-for="cateItem in item.itemList" :key='item.id'>
+					<image :src="cateItem.showPicUrl" mode=""></image>
+					<view>{{cateItem.name}}</view>
+				</view>
+			</scroll-view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import {mapState} from 'vuex'
+	export default {
+		computed: {
+			...mapState({
+				indexData: state => state.indexModule.indexData
+			})
+		}
+	}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+	.cateGoryItem
+		margin-top 20rpx
+		.titlePic
+			width 100%
+			height 370rpx
+		.cateScroll
+			white-space nowrap
+			.scrollItem
+				width 200rpx
+				display inline-block
+				margin-right 20rpx
+				vertical-align top
+				image
+					width 200rpx
+					height 200rpx
+					background #EEEEEE
+				view
+					white-space pre-wrap
+					font-size 26rpx
+					overflow hidden
+					text-overflow ellipsis
+					display -webkit-box
+					-webkit-box-orient vertical
+					-webkit-line-clamp 2
+					
+</style>
