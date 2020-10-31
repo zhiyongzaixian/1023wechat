8fffb98ba474ff030a3b722daa992d08bf48d409
zhiyongzaixian <13852292775@163.com>
2020-10-31 11:54:34 +0800

    '1031'

--- guigushangcheng_study/guigushop/store/index.js
+++ guigushangcheng_study/guigushop/store/index.js
@@ -1,13 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import indexModule from './modules/indexModule'
+import cartModule from './modules/cartModule'
 Vue.use(Vuex);
 
 
 // 1. 创建store对象
 const store = new Vuex.Store({
 	modules:{
-		indexModule
+		indexModule,
+		cartModule
 	}
 })
 
